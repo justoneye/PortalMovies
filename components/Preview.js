7f2914570e0292b041ef37350836a43e127d4f31
Pilar Camargo Márquez <44166916+justoneye@users.noreply.github.com>
2020-07-01 22:15:35 -0500

    Add files via upload
    Creation of Components for RatedMovies.

---
+++ components/Preview.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './bootstrap';
+
+class Lastest extends Component {
+  render() {
+    return (
+      <div className="Lastest">
+        <h1>Lastest Movies</h1>
+        <div className="LastestMoviesContainer">
+        
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Lastest;
