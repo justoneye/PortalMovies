3612c0a159c7a55da3ea4dbc6da320b7f0871319
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-16 09:10:39 -0500

    Filter added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Search  from '../components/Search';
 import Lastest  from '../components/Lastest';
+import Filters  from '../components/Filters';
 import Footer  from '../components/Footer';
 
 import '../components/component_styles.css'
@@ -13,9 +14,14 @@ export default class Home extends React.Component {
       <div className="Page">
         <Search/>
         <Banner/> 
-        <div className="PreviewLastestMovies">
-          <Lastest/> 
-        </div> 
+        <div className="row">
+          <div className="col-3">
+            <Filters/> 
+          </div>
+          <div className="col-9 PreviewLastestMovies">
+            <Lastest/> 
+          </div> 
+        </div>
         <Footer/> 
       </div>
     );
