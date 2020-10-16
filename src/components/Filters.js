53ff9a12554c1ae7d6cbd7bcca15169ee5e7b6d5
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-16 09:10:23 -0500

    Filter section added

---
+++ src/components/Filters.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Filters extends Component {
+  render() {
+    return (
+      <div className="Filters">           
+      </div>
+    );
+  }
+}
+
+export default Filters;
