08268bdd95ce21d8ed5fccd1c01a6d2c8184a536
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-16 09:00:55 -0500

    Text Deleted

--- src/components/Lastest.js
+++ src/components/Lastest.js
@@ -6,8 +6,6 @@ class Lastest extends Component {
   render() {
     return (
       <div className="Lastest">
-        <h1 className="Lastest_Title">Lastest Movies</h1>
-        <p>Check the information of the lastest movies around the world</p>
         <div className="row Lastest_View">
           <Preview/>
         </div>
