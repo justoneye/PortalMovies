0ed4430e6f7bacd14c568b0a7ede98c7d923e045
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-16 09:00:36 -0500

    Footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">           
-        <h5>RatedMovies (C) 2020</h5>
-        <h6>RatedMovies was developed by <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a></h6>       
+        <h5>PortalMovies (C) 2020</h5>
+        <h6>PortalMovies was developed by <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a></h6>       
       </div>
     );
   }
